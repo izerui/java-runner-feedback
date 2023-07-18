f65696664356c403cc2c4a87a7f0e32538ca58b7
serv <liuyuhua69@gmail.com>
2023-07-18 16:10:06 +0800

    runner-feedback-agent.jar

--- runner-feedback-sample/src/main/java/com/github/sample/service/SampleService.java
+++ runner-feedback-sample/src/main/java/com/github/sample/service/SampleService.java
@@ -5,23 +5,22 @@ import com.github.sample.dao.SampleDao;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class SampleService {
 
     private ExecutorService service = Executors.newFixedThreadPool(10);
 
     public String getUserName(String user) throws ExecutionException, InterruptedException {
-//        Future<String> future = service.submit(() -> {
-//            SampleDao sampleDao = new SampleDao();
-//            return sampleDao.getName(user);
-//        });
-//        return future.get();
-        ;
-        ;
-        ;
-        System.out.println("fff");
-        SampleDao sampleDao = new SampleDao();
-        sampleDao.getVoid("ff", true);
-        return "sss";
+        Future<String> future = service.submit(() -> {
+            SampleDao sampleDao = new SampleDao();
+            return sampleDao.getName(user);
+        });
+        return future.get();
+
+//        System.out.println("fff");
+//        SampleDao sampleDao = new SampleDao();
+//        sampleDao.getVoid("ff", true);
+//        return "sss";
     }
 }
