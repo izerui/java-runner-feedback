7e94847faa18c675b571b37234a8c2018e309f96
serv <yifeng2000@126.com>
2023-07-21 00:41:47 +0800

    complete

--- runner-feedback-agent/src/main/java/com/github/izerui/Agent.java
+++ runner-feedback-agent/src/main/java/com/github/izerui/Agent.java
@@ -2,6 +2,7 @@ package com.github.izerui;
 
 import com.github.izerui.ansi.AnsiColor;
 import com.github.izerui.ansi.AnsiOutput;
+import com.github.izerui.context.Context;
 import com.github.izerui.logger.LoggerTransformer;
 import com.github.izerui.structure.StructureTransformer;
 import com.github.izerui.support.Span;
@@ -27,6 +28,8 @@ public class Agent {
         Arrays.stream(PREMAIN_AGENTS).forEach(premainAgent -> premainAgent.premain(args, instrumentation));
         System.out.println("☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟☟");
         System.out.println(AnsiOutput.toString(AnsiColor.BRIGHT_MAGENTA, "插桩: runner-feedback-agent 成功!"));
+        System.out.println(AnsiOutput.toString(AnsiColor.BRIGHT_MAGENTA, "feedback.packages: " + Context.PACKAGES));
+        System.out.println(AnsiOutput.toString(AnsiColor.BRIGHT_MAGENTA, "feedback.deepshow: " + Context.DEEP_SHOW));
         System.out.println(AnsiOutput.toString(AnsiColor.BRIGHT_MAGENTA, "使用@Tracer('标记方法')就可以拦截调用链并输出树状结构!"));
         System.out.println(AnsiOutput.toString(AnsiColor.BRIGHT_MAGENTA, "开始愉快的玩耍吧!!!"));
         System.out.println("☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝☝");
